a041972d3aa9a44e8158cbd51a63b25cb5f4ed63
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-09-25 14:08:39 +0300

    for ephesus

--- components/Login.vue
+++ components/Login.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
-  >
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="w-full max-w-md space-y-8">
       <div>
         <img
@@ -9,9 +7,7 @@
           src="../assets/images/catoon.png"
           alt="Your Company"
         />
-        <h2
-          class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
-        >
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Login in to your account
         </h2>
         <p class="mt-2 text-center text-sm text-gray-600">
@@ -64,9 +60,7 @@
           </div>
 
           <div class="text-sm">
-            <a
-              href="#"
-              class="font-medium text-indigo-600 hover:text-indigo-500"
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"
               >Forgot your password?</a
             >
           </div>
@@ -103,12 +97,10 @@
                 <p class="text-xs text-center">Login</p>
                 <!-- Tooltip here -->
                 <div
-                  class="[transform:perspective(50px)_translateZ(0)_rotateX(10deg)] group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-0 mb-6 origin-bottom transform rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100"
+                  class="[transform:perspective(50px)_translateZ(0)_rotateX(10deg)] group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-0 mb-6 origin-bottom rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100"
                 >
                   <div class="flex max-w-xs flex-col items-center">
-                    <div
-                      class="rounded bg-gray-900 p-2 text-xs text-center shadow-lg"
-                    >
+                    <div class="rounded bg-gray-900 p-2 text-xs text-center shadow-lg">
                       Login
                     </div>
                     <div class="clip-bottom h-2 w-4 bg-gray-900"></div>
@@ -152,12 +144,10 @@
                   <p class="text-xs text-center">Sign in</p>
                   <!-- Tooltip here -->
                   <div
-                    class="[transform:perspective(50px)_translateZ(0)_rotateX(10deg)] group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-0 mb-6 origin-bottom transform rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100"
+                    class="[transform:perspective(50px)_translateZ(0)_rotateX(10deg)] group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-0 mb-6 origin-bottom rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100"
                   >
                     <div class="flex max-w-xs flex-col items-center">
-                      <div
-                        class="rounded bg-gray-900 p-2 text-xs text-center shadow-lg"
-                      >
+                      <div class="rounded bg-gray-900 p-2 text-xs text-center shadow-lg">
                         Sign in
                       </div>
                       <div class="clip-bottom h-2 w-4 bg-gray-900"></div>
