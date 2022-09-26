d3c78e3772abcbd05aad87b4d9cfd765983478be
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-09-26 17:57:13 +0300

    video text reduced

--- pages/videos.vue
+++ pages/videos.vue
@@ -4,7 +4,7 @@
   <section class="bg-gray-900 py-7 px-12">
     <VerticalNav1 />
     <h1
-      class="md:text-3xl text-xl font-mono text-white flex justify-center items-center mx-auto uppercase p-1"
+      class="md:text-3xl text-sm font-mono text-white flex justify-center items-center mx-auto uppercase p-1"
     >
       Messianic Gospel Artist.
     </h1>
